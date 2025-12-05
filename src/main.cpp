3ca6817286e179b1d25c2bc5d7a4098d36453d03
Ayman-AlAmin <e2291718@cmaisonneuve.qc.ca>
2025-12-05 12:27:45 -0500

    Se connecter au WiFi

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,49 @@
 #include <Arduino.h>
+#include <WiFi.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Information de connexion WiFi
+const char* ssid = "IOT-6220";
+const char* password = "6220@Maisonneuve"; // PAS SUR A REVOIR!!!!!!!!!!!!!!
+
+
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  delay(1000);
+  
+  // Connect to WiFi
+  Serial.println();
+  Serial.print("Connexion au WiFi: ");
+  Serial.println(ssid);
+  
+  // Début de la connexion au WiFi de l'école
+  WiFi.begin(ssid, password);
+
+  // Attente de connection avec timeout simple
+  unsigned long startAttemptTime = millis();
+  const unsigned long timeoutMs = 15000; // 15 seconds max
+
+  while (WiFi.status() != WL_CONNECTED && (millis() - startAttemptTime) < timeoutMs) {
+    delay(500);
+    Serial.print(".");
+  }
+
+    // Affichage de l'IP quand connexion réussite 
+  Serial.println();
+
+  if (WiFi.status() != WL_CONNECTED) {
+    Serial.println("Erreur: impossible de se connecter au WiFi. Verifiez le reseau ou le mot de passe.");
+    return;
+  }
+
+  // Affichage de l'IP quand connexion réussite 
+  Serial.println("WiFi connected!");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
